0c8b157c7ea2808cb7816d06c84f38fb5b4abdc1
udin jelek <x.setyori@gmail.com>
2025-01-10 16:18:57 +0700

    add SQLALCHEMY_DATABASE_URI setting

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # config.py
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://polc6219_admin:Bismillah123@203.175.8.151:3306/polc6219_class_gojp'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://tomg9719_api_user:LgMFTy4q3RpzmJy@203.175.9.175:3306/tomg9719_nihongo_hub'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # CORS Settings
     CORS_SETTINGS = {
